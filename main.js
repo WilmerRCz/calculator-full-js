12ad33ad09d41489f2a2d0650ee5a87d42a587b0
Wilmer Rodriguez <wilmerr09@gmail.com>
2023-02-22 11:51:07 -0300

    creacion de buttons y muestra de contenido de input en consola

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
-const app = document.getElementById("app")
+const app = document.getElementById("app");
+const input = document.createElement("input");
+
+document.body.appendChild(input);
+
+function addText(textButton) {
+  input.value += textButton;
+}
+
+function resultOperation() {
+  console.log(input.value);
+}
+
+function createButton(textInButton) {
+  const button = document.createElement("button");
+  const textButton = document.createTextNode(textInButton);
+  button.appendChild(textButton);
+  button.setAttribute("onClick", `addText('${textInButton}')`);
+  document.body.appendChild(button);
+}
+function buttonResult() {
+  const button = document.createElement("button");
+  const textButton = document.createTextNode("=");
+  button.appendChild(textButton);
+  button.setAttribute("onClick", `resultOperation()`);
+  document.body.appendChild(button);
+}
+
+buttonResult();
+createButton("1");
+createButton("2");
+createButton("3");
+createButton("4");
+createButton("5");
+createButton("6");
+createButton("7");
+createButton("8");
+createButton("9");
+createButton("+");
+createButton("-");
+createButton("*");
+createButton("/");
