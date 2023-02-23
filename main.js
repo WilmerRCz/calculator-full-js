ee6f01f2b68df46918c904f8c8ca4447a8cfe08d
Wilmer Rodriguez <wilmerr09@gmail.com>
2023-02-23 19:44:03 -0300

    agregando boton . y del +  modificar estilos

--- main.js
+++ main.js
@@ -20,22 +20,24 @@ grid.appendChild(input)
 
 //Creación de buttons
 const botones = [
-  createButton("1"),
-  createButton("2"),
-  createButton("3"),
-  createButton("+"),
-  createButton("4"),
-  createButton("5"),
-  createButton("6"),
-  createButton("-"),
-  createButton("7"),
-  createButton("8"),
-  createButton("9"),
-  createButton("*"),
-  createButton("0"),
   createButton("clear"),
-  createButton("="),
+  createButton("del"),
+  createButton("+"),
+  createButton("9"),
+  createButton("8"),
+  createButton("7"),
+  createButton("-"),
+  createButton("6"),
+  createButton("5"),
+  createButton("4"),
+  createButton("*"),
+  createButton("3"),
+  createButton("2"),
+  createButton("1"),
   createButton("/"),
+  createButton("."),
+  createButton("0"),
+  createButton("="),
 ]
 
 //Función que agrega texto (Depende de funcion de validar texto)
@@ -59,11 +61,14 @@ function validationInputText(lastChart, textButton, lastPosition) {
   }
   else if (lastChart === '/' && lastChart === textButton) {
     return input.value
-  } else if (lastChart === '+' || lastChart === '-' || lastChart === '*' || lastChart === '/') {
-    if (textButton !== '+' && textButton !== '-' && textButton !== '*' && textButton !== '/') {
+  }  else if (lastChart === '.' && lastChart === textButton) {
+    return input.value
+  }
+  else if (lastChart === '+' || lastChart === '-' || lastChart === '*' || lastChart === '/' || lastChart === '.') {
+    if (textButton !== '+' && textButton !== '-' && textButton !== '*' && textButton !== '/' && textButton !== '.') {
       return input.value += textButton;
     }
-    deleteLastCharAtInInput(lastPosition)
+    verifyLastCharAtIsSimbol(lastPosition)
     return input.value += textButton;
   }
   else {
@@ -71,10 +76,15 @@ function validationInputText(lastChart, textButton, lastPosition) {
   }
 }
 //Función que me elimina el último caracter del input
-function deleteLastCharAtInInput(lastPosition) {
+function verifyLastCharAtIsSimbol(lastPosition) {
   const cutString = input.value.slice(0, lastPosition)
   input.value = cutString
 
+}
+function deleteLastChartInInput(){
+  const cutString = input.value.slice(0, -1)
+  input.value = cutString
+  return input.value
 }
 //Función que ejecuta lo que esta ingresado en el input como operación matemática
 function resultOperation() {
@@ -91,13 +101,17 @@ function resultOperation() {
 function createButton(textInButton) {
   const button = document.createElement("button");
   const textButton = document.createTextNode(textInButton);
-  button.setAttribute("class", "text-white font-semibold  col-span-1 uppercase rounded-lg px-2 py-4 bg-gradient-to-r from-blue-500 to-pink-500 md:hover:from-pink-500 md:hover:to-amber-500")
+  button.setAttribute("class", "text-white font-semibold  col-span-1 uppercase rounded-xl px-2 py-4 bg-gradient-to-r from-blue-500 to-pink-500 ")
   button.appendChild(textButton);
   if (textInButton === '=') {
+    button.setAttribute("class", `text-white font-bold text-3xl col-span-2 uppercase rounded-xl bg-gradient-to-r from-amber-500 to-pink-500`);
     button.setAttribute("onClick", `resultOperation()`);
   } else if (textInButton === 'clear') {
+    button.setAttribute("class", `text-white font-semibold  col-span-2 uppercase rounded-xl px-2 py-4 bg-gradient-to-r from-blue-500 to-pink-500`);
     button.setAttribute("onClick", `input.value = ''; result.innerText = ''`);
-  } else {
+  } else if (textInButton === 'del') {
+    button.setAttribute("onClick", `deleteLastChartInInput()`);
+  }else {
     button.setAttribute("onClick", `addText('${textInButton}')`);
   }
   grid.appendChild(button);
